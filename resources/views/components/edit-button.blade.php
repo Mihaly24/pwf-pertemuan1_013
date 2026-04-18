3d521f7d5a9a64661f73f2b036baaa7295be0907
Mihaly24 <fairuzk20@gmail.com>
2026-04-18 09:29:39 +0700

    Pertemuan 7 - Add components

---
+++ resources/views/components/edit-button.blade.php
@@ -0,0 +1,7 @@
+<a href="{{ $url }}" 
+   class="inline-flex items-center px-3 py-1 bg-yellow-100 text-yellow-800 font-medium rounded-lg hover:bg-yellow-200 transition ease-in-out duration-150">
+    <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
+    </svg>
+    Edit
+</a>
--- resources/views/produk/index.blade.php
+++ resources/views/produk/index.blade.php
@@ -40,24 +40,11 @@
                                     <td class="px-6 py-4 text-sm text-center">
                                         <div class="flex items-center justify-center space-x-3">
                                             @can('update', $product)
-                                                <a href="{{ route('produk.edit', $product) }}" class="inline-flex items-center px-3 py-1 bg-yellow-100 text-yellow-800 font-medium rounded-lg hover:bg-yellow-200 transition ease-in-out duration-150">
-                                                    <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
-                                                    </svg>
-                                                    Edit
-                                                </a>
+                                                <x-edit-button :url="route('produk.edit', $product)" />
                                             @endcan
                                             
                                             @can('delete', $product)
-                                                <form action="{{ route('produk.destroy', $product) }}" method="POST" class="inline" onsubmit="return confirm('Apakah Anda yakin ingin menghapus produk ini?');">
-                                                    @csrf @method('DELETE')
-                                                    <button type="submit" class="inline-flex items-center px-3 py-1 bg-red-100 text-red-800 font-medium rounded-lg hover:bg-red-200 transition ease-in-out duration-150">
-                                                        <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
-                                                        </svg>
-                                                        Hapus
-                                                    </button>
-                                                </form>
+                                                <x-delete-button :url="route('produk.destroy', $product)" />
                                             @endcan
                                         </div>
                                     </td>
